cb642c5119cbcd3800e9e81c66d3e86aabd2a13a
Gaurav2912 <yogeshwar.gaurav0@gmail.com>
2024-08-08 16:34:49 +0530

    local to global

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
 import os
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def play():
     return {'filename': file.filename}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
